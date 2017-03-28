4e38d50779ba8730183c5b15f6d85b11d4f51c07
xqgtiti <xqgtiti@163.com>
2017-03-28 18:48:18 +0800

    Add files via upload

---
+++ sh1.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+# NOTE: This is an example that sets up SSH authorization. To use it, you'd need to replace "ssh-rsa AA... youremail@example.com" with your SSH public.
+# You can replace this entire script with anything you'd like, there is no need to keep it
+
+
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH
+sudo apt-get update -y
+sudo apt-get install -y language-pack-zh-hant language-pack-zh-hans
+sudo apt install -y python
+sudo apt install -y python-pip
+pip install requests
+pip install pymysql
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password zhuzhu66'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password zhuzhu66'
+sudo apt-get install -y mysql-server
+sudo apt-get install -y mysql-client
+sudo apt-get install -y libmysqlclient-dev
+exit 0
